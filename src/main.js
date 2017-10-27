4adf087003bf068039f767c419dc7be5282e18a3
theteerapon <theteerapon11@gmail.com>
2017-10-27 21:51:56 +0700

    gg

--- src/main.js
+++ src/main.js
@@ -45,9 +45,9 @@ class LINE extends LineAPI {
         }
 
         if(operation.type == 19) { //ada kick
-            // op1 = group nya
-            // op2 = yang 'nge' kick
-            // op3 = yang 'di' kick
+             op1 = group nya
+             op2 = yang 'nge' kick
+             op3 = yang 'di' kick
             if(isAdminOrBot(operation.param3)) {
                 this._invite(operation.param1,[operation.param3]);
             }
